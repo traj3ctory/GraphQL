3287d9698c9095399dce5ad236ed760b0f3eebd7
traj3ctory <davidbenard.bd@gmail.com>
2021-02-22 13:45:08 +0100

    initial update

--- models/author.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const authorScema = new Schema({
-    name: String,
-    age: Number,
-});
-
-module.exports = mongoose.model("Author", authorScema);
